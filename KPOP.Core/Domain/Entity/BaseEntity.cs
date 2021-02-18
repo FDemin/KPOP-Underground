1defc649a6d29e73d5715838fca9757578ae839b
Francis Demin <francisdemin@hotmail.com>
2021-02-19 01:07:39 +0800

    Added Web Project

---
+++ KPOP.Core/Domain/Entity/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPOP.Core.Domain.Entity
+{
+    public abstract class BaseEntity
+    {
+        public int ID { get; protected set; }
+
+        protected BaseEntity()
+        {
+
+        }
+    }
+}
--- KUG.Core/Class1.cs
+++ KUG.Core/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KPOP.Core
+namespace KUG.Core
 {
     public class Class1
     {
