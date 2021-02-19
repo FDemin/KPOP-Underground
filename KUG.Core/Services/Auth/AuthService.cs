f59465cbd6046accaa8d6efd79eca7273fa0d278
Francis Demin <francisdemin@hotmail.com>
2021-02-19 11:37:02 +0800

    Added Service Files

---
+++ KUG.Core/Services/Auth/AuthService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KUG.Core.Services.Auth
+{
+    public class AuthService : IAuthService
+    {
+        public readonly string connectionString;
+
+        public AuthService(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        bool IAuthService.Authenticate(string username, string password)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("Authenticate", conn) { CommandType = System.Data.CommandType.StoredProcedure })
+            {
+                conn.Open();
+
+                command.Parameters.AddWithValue("@Username", username.Trim());
+                command.Parameters.AddWithValue("@Password", password.Trim());
+
+                return (int)command.ExecuteScalar() == 1;
+            }
+        }
+
+        bool IAuthService.UserExists(string username)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("UserExists", conn) { CommandType = System.Data.CommandType.StoredProcedure })
+            {
+                conn.Open();
+
+                command.Parameters.AddWithValue("@Username", username.Trim());
+
+                return (int)command.ExecuteScalar() == 1;
+            }
+        }
+
+        int IAuthService.GetUser(string username, string password)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("GetUser", conn) { CommandType = System.Data.CommandType.StoredProcedure })
+            {
+                conn.Open();
+
+                command.Parameters.AddWithValue("@Username", username.Trim());
+                command.Parameters.AddWithValue("@Password", password.Trim());
+
+                using(var rdr = command.ExecuteReader())
+                {
+                    rdr.Read();
+
+                    return rdr.GetInt32(rdr.GetOrdinal("UserId"));
+                }
+            }
+        }
+
+        void IAuthService.CreateAccount(string username, string password, string fullName, string email, string contactNumber)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("CreateAccount", conn) { CommandType = System.Data.CommandType.StoredProcedure })
+            {
+                conn.Open();
+
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+                command.Parameters.AddWithValue("@Name", fullName);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Number", contactNumber);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ KUG.Core/Services/Auth/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿namespace KUG.Core.Services.Auth
+{
+    public interface IAuthService
+    {
+        bool Authenticate(string username, string password);
+        bool UserExists(string username);
+        int GetUser(string username, string password);
+        void CreateAccount(string username, string password, string fullName, string email, string contactNumber);
+    }
+}
